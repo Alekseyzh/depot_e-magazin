e349bf8ba9f833c975c0c0fa7298db078465f216
Alekseyzh <allbeehappy@gmail.com>
2013-07-29 15:05:20 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IMagazine::Application.config.secret_token = 'eab40bbfb283149f735110be824d26f6aff2fbf536cd07a25b166eedd43f507115393d75da3f8b2668c8378186a09d6e85d8bff1bc442e1d59950ee1ada759e3'
